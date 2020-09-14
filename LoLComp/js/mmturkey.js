02422b69186bb5a235faf90e66992f08b5f26c12
jcoon1 <60113447+jcoon1@users.noreply.github.com>
2020-09-13 22:06:48 -0700

    data path fix

--- LoLComp/js/mmturkey.js
+++ LoLComp/js/mmturkey.js
@@ -125,7 +125,7 @@ turk = turk || {};
       // document.body.appendChild(div);
       $.ajax({
           type: 'POST',
-          url: 'https://bayes.langsci.uci.edu/meaninglab/jcoon1/LoLPilot/LoLPilot/submit.php',
+          url: 'https://bayes.langsci.uci.edu/meaninglab/jcoon1/LoLComp/LoLComp/submit.php',
           dataType: 'json',
           data: {'jsonData': JSON.stringify(rawData)}
       });
